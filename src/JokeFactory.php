dec855d6ee89cc66e8053f5aa605e5d237514c2d
Jodie Buggea <jodie@pdc.is>
2019-10-04 10:53:05 +0100

    test

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -5,25 +5,19 @@ namespace jodiebuggea\ChuckNorrisJokes;
 
 use GuzzleHttp\Client;
 
-class JokeFactory
 
+class JokeFactory
 {
     const API_ENDPOINT = 'http://api.icndb.com/jokes/random';
-
     protected $client;
-
     public function __construct(Client $client = null)
     {
         $this->client = $client ?: new Client();
     }
-
     public function getRandomJoke()
     {
         $response = $this->client->get(self::API_ENDPOINT);
-
-        $joke =  json_decode($response->getBody()->getContents());
-
+        $joke = json_decode($response->getBody()->getContents());
         return $joke->value->joke;
     }
-
 }
--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -2,27 +2,24 @@
 
 namespace jodiebuggea\ChuckNorrisJokes\Jokes;
 
-use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\Client;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
-use http\Client;
-use jodiebuggea\ChuckNorrisJokes\JokeFactory;
 use PHPUnit\Framework\TestCase;
-
+use GuzzleHttp\Handler\MockHandler;
+use jodiebuggea\ChuckNorrisJokes\JokeFactory;
 class JokeFactoryTest extends TestCase
 {
-/** @test  */
-
-public function it_returns_a_random_joke()
-{
-    $mock = new MockHandler([
-       new Response(200, [],'{ "type": "success", "value": { "id": 268, "joke": "Time waits for no man. Unless that man is Chuck Norris." } }' ),
-    ]);
-    $handler = HandlerStack::create($mock);
-    $client = new \GuzzleHttp\Client();
-    $jokes = new JokeFactory($client);
-    $joke = $jokes->getRandomJoke();
-
-    $this->assertSame('Time waits for no man. Unless that man is Chuck Norris.', $joke);
-}
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $mock = new MockHandler([
+            new Response(200, [], '{ "type": "success", "value": { "id": 441, "joke": "Chuck Norris did not &quot;lose&quot; his virginity, he stalked it and then destroyed it with extreme prejudice.", "categories": [] } }'),
+        ]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+        $jokes = new JokeFactory($client);
+        $joke = $jokes->getRandomJoke();
+        $this->assertSame('Chuck Norris did not &quot;lose&quot; his virginity, he stalked it and then destroyed it with extreme prejudice.', $joke);
+    }
 }
